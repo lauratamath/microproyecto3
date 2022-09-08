a087fa60bba8c00d7cce5b33022466502b271138
Martín España <46570760+martspain@users.noreply.github.com>
2022-09-08 08:28:24 -0600

    Started exercise 4

---
+++ src/eje4.py
@@ -0,0 +1,48 @@
+from random import random, seed
+
+# seed(1029384756)
+
+# 9  - 0.3 prob
+# 10 - 0.4 prob
+# 11 - 0.3 prob
+
+probDist = {"9":0.3, "10":0.4, "11":0.3}
+
+def simulate(newspapers, days):
+
+  simActives = 0
+  simDebt = 0
+  simulatedResult = 0
+
+  for i in range(days):
+
+    randNum = random()
+
+    options = []
+    probs = []
+
+    simPapers = 0
+
+    for item in probDist.keys():
+      options.append(item)
+      probs.append(probDist[item])
+    
+    currentProb = probs[0]
+
+    # print("rand: ", randNum)
+
+    for p in range(len(probs)):
+      if randNum <= currentProb:
+        simPapers = int(options[p])
+        break
+      if p+1 < len(probs):
+        currentProb += probs[p+1]
+    
+    # print("Simulated: ", simPapers)
+    simDebt += simPapers * 1.5 # Price paid for each paper
+
+    if newspapers < simPapers:
+      simActives += (simPapers - newspapers) * 0.5 # Refund
+    simActives += simPapers * 2.5
+
+simulate(9, 10)
